09a63eaf17909f55a2b48d3329aeedba21d1851e
FelixRR <f@felixrr.pro>
2016-06-14 19:12:14 +0100

    Fixing the exit routine so that the whole application actually closes when told to do so by the operator.

--- Responder.py
+++ Responder.py
@@ -272,12 +272,4 @@ def main():
 		poisoners.HTML.sys.exit("\r%s Exiting..." % color('[+]', 2, 1))
 
 if __name__ == '__main__':
-	try:
 		main()
-	except KeyboardInterrupt:
-		print 'User signaled exit...'
-		try:
-			sys.exit(0)
-		except SystemExit:
-			os._exit(0)
-
